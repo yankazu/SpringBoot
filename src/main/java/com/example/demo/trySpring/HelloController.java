629e9fc3a84db4f9ad48ff1afe5811adf4784f87
yankazu <wittgenstein289156@yahoo.co.jp>
2020-06-30 22:18:21 +0900

    20200630

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +10,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+	@Autowired
+	private HelloService helloService;
+
 	@GetMapping("/hello")
 	public String getHello() {
+		// hello.htmlに遷移
 		return "hello";
 	}
 
 	@PostMapping("/hello")
 	public String postRequest(@RequestParam("text1")String str, Model model) {
+		// 画面から受け取った文字列をModelに登録
 		model.addAttribute("sample",str);
-
+		// helloResponse.htmlに画面遷移
 		return "helloResponse";
 	}
+
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String str, Model model) {
+
+		// StringからInt型へ変換
+		int id = Integer.parseInt(str);
+
+		// 1件検索
+		Employee employee = helloService.findOne(id);
+
+		// 検索結果をModelに登録
+		model.addAttribute("id",employee.getEmployeeId());
+		model.addAttribute("name",employee.getEmployeeName());
+		model.addAttribute("age",employee.getAge());
+
+
+		// helloResponseDB.htmlに画面遷移
+		return "helloResponse";
+	}
+
 
 }
